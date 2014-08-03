09cfce20b368016a3f175de831efe32682601291
thepeg <marco.pegoraro@gmail.com>
2014-08-03 14:44:12 +0200

    upgraded to use noblackmagic template

---
+++ assets/js/polite.js
@@ -0,0 +1,12 @@
+
+(function(){
+    var html =  '<div id="header-block" class="clearfix">' +
+                    '<img id="logo" class="left" alt="PoliteJS Logo" width="75" src="//politejs.com/assets/images/polite.jpg" />' +
+                    '<div id="title-block" class="left">' +
+                        '<h1>PoliteJS<small>.com</small></h1>' +
+                        '<p>that\'s web stuff</p>' +
+                    '</div>' +
+                '</div>';
+    
+    document.write(html);
+})();
